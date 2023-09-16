486dd83d166d12c21b447c9358a880f79d3490f4
OnajiteChristabel <christabelonajite2005@gmail.com>
2023-09-16 04:16:57 -0700

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const submitButton = document.querySelector('#submit'); 
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
+})
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=c57ce337&app_key=ba6e361b0c23e673862bb48f081afc87&from=0&to=10`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
